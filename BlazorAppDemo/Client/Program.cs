7b6ac162facb98c785971e3f236766fc477d56ff
SPYROS\Spyros <sponaris@gmail.com>
2024-02-16 21:06:07 +0200

    Add project files.

---
+++ BlazorAppDemo/Client/Program.cs
@@ -0,0 +1,12 @@
+using BlazorAppDemo.Client;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7195/") });
+
+await builder.Build().RunAsync();
